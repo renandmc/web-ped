b000e534a802e14115549d9696ebd26a8c9a49b1
Renan da Mota Ciciliato <renanciciliato@gmail.com>
2022-03-08 00:42:27 -0300

    CRUD empresas: - Criação base: migration, model, factory, seeder. policy, requests-validations

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*
+        DB::table('users')->insert([
+            'name' => 'Renan da Mota Ciciliato',
+            'email' => 'renanciciliato@gmail.com',
+            'password' => Hash::make('@renandmc93'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        */
+    }
+}
